510b1e456772c4bfb81fe3ab8966cfe583d88724
thanhtoan2588 <thanhtoan2588@gmail.com>
2024-03-21 15:08:44 +0700

    footer

--- my-app/src/components/Login.jsx
+++ my-app/src/components/Login.jsx
@@ -1,8 +1,122 @@
 import React from 'react'
+import '../styles/login.css'
+
+import { FaFacebook } from "react-icons/fa";
+import { FaGooglePlus } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup' 
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const loginSchema = yup.object({
+  name: yup.string().required().min(3),
+  password: yup.string().required().min(3),
+});
 
 function Login() {
+
+  const navigate = useNavigate()
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm ({
+    mode: 'onChange',
+    resolver: yupResolver(loginSchema)
+  })
+
+  const handleFormLogin = (data) =>{
+
+    const dataUser = JSON.parse(localStorage.getItem("dataRegister"))
+
+    if(data.name === dataUser.name && data.password === dataUser.password){
+
+      toast.success("Đăng nhập thành công", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+
+      setTimeout(() => {
+        navigate("/")
+      },2500)
+
+    }else{
+
+      toast.error("Đăng nhập thất bại", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        });
+
+    }
+
+  }
+
   return (
-    <div>Login</div>
+    <>
+      <div class="login-container">
+        <div>
+        <div class="title-container">
+          <h1>CIT EDU SHOP</h1>
+          <p>Khách hàng</p>
+          <button><a className='title-container-button' onClick={() => navigate("/register")}>Register</a></button>
+        </div>
+
+        <div class="form-container">
+          <form onSubmit={handleSubmit(handleFormLogin)}>
+            <h1>Sign In</h1>
+            <div class="social-container">
+                  <a href="#" class="social"><FaFacebook /></a>
+                  <a href="#" class="social"><FaGooglePlus /></a>
+                  <a href="#" class="social"><FaLinkedin /></a>
+            </div>
+            <span>or use your account </span>
+            <input type="text" placeholder="Name" {...register("name")} />
+            <span>{errors.name?.message}</span>
+            <input type="password" placeholder="Password" {...register("password")} />
+            <span>{errors.password?.message}</span>
+            <a href="#">Forgot your password?</a>
+            <p>
+              DON'T HAVE AN ACCOUNT ? <a href="./register.html">REGISTER NOW</a>
+            </p>
+            <button>Login</button>
+          </form>
+        </div>
+        </div>
+    </div>
+    
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
+
+    </>
   )
 }
 
--- my-app/src/components/Register.jsx
+++ my-app/src/components/Register.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import '../styles/register.css'
+
 import { FaFacebook } from "react-icons/fa";
 import { FaGooglePlus } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup' 
+import { useNavigate } from "react-router-dom";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import axios from 'axios';
 import { useForm } from "react-hook-form"
@@ -12,12 +18,14 @@ import { useForm } from "react-hook-form"
 const urlApi = "https://656708c964fcff8d730f9b4b.mockapi.io/api/login/users"
 
 const registerSchema = yup.object({
-  name: yup.string().required(),
+  name: yup.string().required().min(3),
   email: yup.string().email().required(),
-  password: yup.string().required()
+  password: yup.string().required().min(3),
 });
 
 function Register() {
+
+  const navigate = useNavigate()
   
   const [ dataRegister , setDataRegister ] = useState({})
 
@@ -38,50 +46,82 @@ function Register() {
  
     postUser.then(
       (data) => {setDataRegister(data.data);
-      localStorage.setItem("dataRegister", JSON.stringify(data.data))
 
-      alert("đăng ký thành công")
+      localStorage.setItem("dataRegister", JSON.stringify(data.data))
+      // localStorage.setItem("dataRegister", JSON.stringify(dataRegister))
+      // theo em nghỉ khi mình lưu data.data vào localStorage thì nó có đảm bảo dữ liệu mới nhất 
+      // khi cập nhật và không bị mất còn khi em test lưu vào dataRegister thì dữ liệu trong localStogare
+      // không được đẩm bảo và bị rỗng
       }
     )
     postUser.catch(
       (error) => console.log(error)
     )
-
+  
     reset()
+
+    toast.success("Đăng ký thành công", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+    
+   setTimeout(() => {
+    navigate("/login")
+   }, 2500);
 
   }
 
   return (
-    <div class="register-container">
-      <div>
-        <div class="title-container">
-          <h1>CIT EDU SHOP</h1>
-          <p>Đăng nhập để không bỏ lỡ quyền lợi tích luỹ và hoàn tiền
-              cho bất kỳ đơn hàng nào.</p>
-          <button><a href="./login.html">Login</a></button>
-        </div>
+    <>
+        <div class="register-container">
+          <div>
+            <div class="title-container">
+              <h1>CIT EDU SHOP</h1>
+              <p>Đăng nhập để không bỏ lỡ quyền lợi tích luỹ và hoàn tiền
+                  cho bất kỳ đơn hàng nào.</p>
+              <button><a onClick={() => navigate("/login")} >Login</a></button>
+            </div>
 
-      <div class="form-container">
-        <form onSubmit={handleSubmit(handleRegister)}>
-          <h1>Create Account</h1>
-          <div class="social-container">
-            <a href="#" class="social"><FaFacebook /></a>
-            <a href="#" class="social"><FaGooglePlus /></a>
-            <a href="#" class="social"><FaLinkedin /></a>
+          <div class="form-container">
+            <form onSubmit={handleSubmit(handleRegister)}>
+              <h1>Create Account</h1>
+              <div class="social-container">
+                <a href="#" class="social"><FaFacebook /></a>
+                <a href="#" class="social"><FaGooglePlus /></a>
+                <a href="#" class="social"><FaLinkedin /></a>
+              </div>
+              <span>or use your email for registration</span>
+              <input type="text" placeholder="Name" {...register("name")}/>
+              <span>{errors.name?.message}</span>
+              <input type="email" placeholder="Email" {...register("email")}/>
+              <span>{errors.email?.message}</span>
+              <input type="password" placeholder="Password" {...register("password")}/>
+              <span>{errors.password?.message}</span>
+              {/* <p>ALREADY HAVE AN ACCOUNT ? <a>lOGIN NOW</a></p> */}
+              <button>Register</button>
+            </form>
           </div>
-          <span>or use your email for registration</span>
-          <input type="text" placeholder="Name" {...register("name")}/>
-          <p>{errors.email?.name}</p>
-          <input type="email" placeholder="Email" {...register("email")}/>
-          <p>{errors.email?.message}</p>
-          <input type="password" placeholder="Password" {...register("password")}/>
-          <p>{errors.password?.message}</p>
-          <p>ALREADY HAVE AN ACCOUNT ? <a href="./login.html">lOGIN NOW</a></p>
-          <button>Register</button>
-        </form>
+          </div>
       </div>
-      </div>
-  </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   )
 }
 
